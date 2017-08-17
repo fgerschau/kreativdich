af118820ee5be554e0fa31d277522937f07c1927
Felix <felix.gerschau@gmail.com>
2017-08-17 15:27:02 +0200

    Add blogList view

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,43 @@
+'use strict';
+const Prismic = require('prismic-nodejs');
+
 module.exports = function (app) {
   app.route('/').get(function(req, res) {
     const ctx = res.locals.ctx;
     req.prismic.api.getSingle('homepage').then((prismicdoc) => {
       res.render('index', { pagecontent: prismicdoc, ctx });
+    });
+  });
+
+  app.route('/blog').get(function(req, res) {
+    if (!req.query.page) {
+      return res.redirect('/blog?page=1');
+    }
+
+    const blogType = Prismic.Predicates.at('document.type', 'blog-post');
+    const options = {
+      pageSize: 6,
+      page: req.query.page || 1,
+      orderings: '[document.first_publication_date desc]',
+    };
+
+    req.prismic.api.query(blogType, options).then((prismicdoc) => {
+      const posts = [];
+      for (let i = 0; i < prismicdoc.results.length; i++) {
+        const title = prismicdoc.results[i].getText('blog-post.title');
+        const image = prismicdoc.results[i].getImage('blog-post.title-image');
+        const imageUrl = image ? image.url : null;
+        posts.push({ title, imageUrl });
+      }
+
+      const paginationOptions = {
+        page: prismicdoc.page,
+        pageSize: prismicdoc.results_per_page,
+        results: prismicdoc.total_results_size,
+        totalPages: prismicdoc.total_pages,
+      };
+
+      res.render('blogList', { posts, paginationOptions });
     });
   });
 
