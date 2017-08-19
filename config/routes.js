e94c939a23459f6804bf85fe3f2e1161d1fd9527
Felix <felix.gerschau@gmail.com>
2017-08-19 14:43:49 +0200

    Add blog post view and link by uid

--- config/routes.js
+++ config/routes.js
@@ -2,14 +2,14 @@
 const Prismic = require('prismic-nodejs');
 
 module.exports = function (app) {
-  app.route('/').get(function(req, res) {
+  app.route('/').get((req, res) => {
     const ctx = res.locals.ctx;
     req.prismic.api.getSingle('homepage').then((prismicdoc) => {
       res.render('index', { pagecontent: prismicdoc, ctx });
     });
   });
 
-  app.route('/blog').get(function(req, res) {
+  app.route('/blog').get((req, res) => {
     if (!req.query.page) {
       return res.redirect('/blog?page=1');
     }
@@ -27,7 +27,8 @@ module.exports = function (app) {
         const title = prismicdoc.results[i].getText('blog-post.title');
         const image = prismicdoc.results[i].getImage('blog-post.title-image');
         const imageUrl = image ? image.url : null;
-        posts.push({ title, imageUrl });
+        const uid = prismicdoc.results[i].uid;
+        posts.push({ title, imageUrl, uid });
       }
 
       const paginationOptions = {
@@ -38,6 +39,19 @@ module.exports = function (app) {
       };
 
       res.render('blogList', { posts, paginationOptions });
+    });
+  });
+
+  app.route('/blog/:uid').get((req, res) => {
+    req.prismic.api.getByUID('blog-post', req.params.uid).then((prismicdoc) => {
+      const post = {
+        title: prismicdoc.getText('blog-post.title'),
+        imageUrl: prismicdoc.getImage('blog-post.title-image') ? prismicdoc.getImage('blog-post.title-image').url : '',
+        text: prismicdoc.getStructuredText('blog-post.text') ? prismicdoc.getStructuredText('blog-post.text').asHtml() : null,
+        keywords: prismicdoc.getText('blog-post.keywords') || null,
+      }
+
+      res.render('blog', { post });
     });
   });
 
